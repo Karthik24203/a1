58a43b5b70d2ea192e75e6042a45fc66c803bfa6
Karthik V <karthikv24203@gmail.com>
2025-05-15 23:39:50 +0530

    first commit

--- amazon-backend/server.js
+++ amazon-backend/server.js
@@ -1,41 +1,45 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import userRouter from './routers/userRouter.js'
-import productRouter from './routers/productRouter.js'
-import dotenv from 'dotenv'
-import orderRouter from './routers/orderRouter.js'
-
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
+import dotenv from "dotenv";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 const connection_url = process.env.MONGO_URL;
 
-mongoose.connect(connection_url,{
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-
-
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // or your deployed frontend URL
+    credentials: true, // if you're sending cookies/auth headers
+  })
+);
 
-
+// (Optional but recommended for full preflight support)
+app.options("*", cors());
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
-app.get('/api/config/paypal', (req,res)=>{
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-})
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 
-app.get('/',(req,res)=>res.status(200).send('Hello Debjit here. It is Amazon clone project.'))
-
+app.get("/", (req, res) =>
+  res.status(200).send("Hello Debjit here. It is Amazon clone project.")
+);
 
 // Listening to  server
 
-app.listen(port,()=>console.log(`Listening on local host:${port}`))
+app.listen(port, () => console.log(`Listening on local host:${port}`));
